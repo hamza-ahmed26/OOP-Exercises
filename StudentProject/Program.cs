9b1d4f9650a42d25056a8df1cff4683045f1ecbf
Hamza Ahmed <250721788+hamza-ahmed26@users.noreply.github.com>
2026-08-14 05:56:08 +0300

    Add files via upload

---
+++ StudentProject/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudentProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+
+            Student student1 = new Student("Ahmed Ali", 101);
+            student1.AddGrade(85);
+            student1.AddGrade(90);
+            student1.AddGrade(78);
+            students.Add(student1);
+
+            Student student2 = new Student("hala Mohamed", 102);
+            student2.AddGrade(95);
+            student2.AddGrade(88);
+            students.Add(student2);
+
+            foreach (var student in students)
+            {
+                Console.WriteLine($"Student: {student.Name} (ID: {student.Id})");
+                Console.WriteLine($"Average Grade: {student.CalculateAverageGrade():F2}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ StudentProject/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentProject
+{
+    internal class Student
+    {
+        public string Name { get; private set; }
+        public int Id { get; private set; }
+        public List<double> Grades { get; private set; }
+
+        public Student(string name, int id)
+        {
+            Name = name;
+            Id = id;
+            Grades = new List<double>();
+        }
+        public void AddGrade(double grade)
+        {
+            Grades.Add(grade);
+        }
+
+        public double CalculateAverageGrade()
+        {
+            if (Grades.Count == 0)
+                return 0;
+
+            double sum = 0;
+            foreach (double grade in Grades)
+            {
+                sum += grade;
+            }
+
+            return sum / Grades.Count;
+        }
+    }
+}
+
